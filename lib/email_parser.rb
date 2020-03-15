8a1ae132d89d8e56ce0ea88d1b4e2acb74bc88af
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-15 20:16:40 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,3 +9,19 @@ def parse
   returnArr
 end
 end
+
+class EmailAddressParser
+
+  attr_accessor :email_addresses
+
+  def initialize(email_addresses)
+    @email_addresses = email_addresses
+  end
+
+  def parse
+
+    email_addresses.split(/[,\s]+/).uniq
+
+  end
+
+end
